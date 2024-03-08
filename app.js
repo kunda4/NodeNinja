da93fcb211d80603826ad3d3af8aa59a4d554158
Agnes Iradukunda <kundaaggy@gmail.com>
2024-03-08 17:05:52 +0200

    add nav partials

--- app.js
+++ app.js
@@ -9,18 +9,24 @@ app.set('view engine', 'ejs');
 app.listen(3000)
 
 app.get('/', (req, res)=>{
-    res.render('index')
+    const blogs = [
+        {title: 'the title one', snippet: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.'},
+        {title: 'the title one', snippet: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.'},
+        {title: 'the title one', snippet: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.'},
+        {title: 'the title one', snippet: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.'}
+    ]
+    res.render('index', {title:'home', blogs})
 })
 
 app.get('/about', (req, res)=>{
-    res.render('about')
+    res.render('about', {title:'about'})
 })
 
 app.get('/about-us', (req, res)=>{
-    res.redirect('/about')
+    res.redirect('/about', {title:'about'})
 })
 
 //404 page
 app.use((req, res)=>{
-    res.render('404')
+    res.render('404', {title:'404'})
 })
